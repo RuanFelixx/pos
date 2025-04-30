6a3d7a70b2ea6c9443221740e3d7ae269e9fd7c4
RuanFelixx <ruan.felix2024@gmail.com>
2025-04-30 16:22:18 -0300

    add fucionalidades e rotas

--- pos - aula02/main.py
+++ pos - aula02/main.py
@@ -1,26 +1,40 @@
-# pip install fastapi uvicorn pydantic
-# pip freeze > requirements.txt
-# uvicorn main:app
-from fastapi import FastAPI,HTTPException
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 from models import Tarefa
 from typing import List
+from fastapi import status
 
 app = FastAPI()
 
-tarefas:List[Tarefa]=[]
+app.mount("/css", StaticFiles(directory="css"), name="css")
 
-@app.get("/tarefas/",response_model=List[Tarefa])
+templates = Jinja2Templates(directory="templates")
+
+tarefas: List[Tarefa] = []
+
+@app.get("/", response_class=HTMLResponse)
+def exibir_formulario(request: Request):
+    return templates.TemplateResponse("cadastrar_tarefa.html", {"request": request})
+
+@app.get("/listar", response_class=HTMLResponse)
+def exibir_listagem(request: Request):
+    return templates.TemplateResponse("listar_tarefa.html", {"request": request})
+
+@app.get("/tarefas/", response_model=List[Tarefa])
 def listar_tarefas():
     return tarefas
 
-@app.get("/tarefas/{id}",response_model=Tarefa)
-def listar_tarefa(id:int):
-    for tarefa in tarefas:
-        if tarefa.id == id:
-            return tarefa
-    raise HTTPException(status_code=404,detail="Não Localizado")
-
-@app.post("/tarefas/",response_model=Tarefa)
-def criar_tarefa(tarefa:Tarefa):
+@app.post("/tarefas/", response_model=Tarefa)
+def criar_tarefa(tarefa: Tarefa):
     tarefas.append(tarefa)
     return tarefa
+
+@app.delete("/tarefas/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def deletar_tarefa(id: int):
+    for i, tarefa in enumerate(tarefas):
+        if tarefa.id == id:
+            del tarefas[i]
+            return
+    raise HTTPException(status_code=404, detail="Tarefa não encontrada")
